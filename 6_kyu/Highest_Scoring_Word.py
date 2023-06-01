54b9e9435aaf5a45d908bc3e03e3421de1d08671
Augusto Pareja <augustopareja@gmail.com>
2023-06-01 14:26:22 -0300

    Add Highest_Scoring_Word py sample script

---
+++ 6_kyu/Highest_Scoring_Word.py
@@ -0,0 +1,16 @@
+alphabet = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12,  "m": 13, "n": 14, 
+            "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26}
+
+def get_value(word):
+    return sum(alphabet[letter] for letter in word)
+
+def high(word_string):
+    return max(word_string.split(), key=get_value)
+
+if __name__ == '__main__':
+    word = 'qytidud tfgsboulu bcotde scsxigulft atmkeavo qhfrlug rrozrmrjd jolnkw mqymeonz ayspegyf'
+    print(f"The highest word high in '{word}' is {high(word)}")
+    word = 'nzeuuw zjqsaidfn efqwj mqdncge onpuyy hfbich fkzbcedddk'
+    print(f"The highest word high in '{word}' is {high(word)}")
+    word = 'shcrl irrresk gfieoi xao hcfldkqmtq qaggbsatc pumo dcei'
+    print(f"The highest word high in '{word}' is {high(word)}")
